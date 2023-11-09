9677e2f2713d954c1f1dc562df3fb0c34cbd1f75
Glory (Ada) Ogbondah <113802790+MsGeeO@users.noreply.github.com>
2023-11-09 13:31:02 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,16 +8,7 @@ import { initializeAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyAPbi5-xdX8s3h7aK_0S38z1VjY4kfGSk4',
-  authDomain: 'crowd-care.firebaseapp.com',
-  databaseURL: 'https://crowd-care-default-rtdb.firebaseio.com',
-  projectId: 'crowd-care',
-  storageBucket: 'crowd-care.appspot.com',
-  messagingSenderId: '457828323552',
-  appId: '1:457828323552:web:71b6458d2dea63b0a68085',
-  measurementId: 'G-HX6P5JD5Y9'
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
